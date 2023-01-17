8afcc0a80dbfcd68328b9a7c7a652e5c42f5c8ce
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-17 12:51:44 +0100

    Started Styling the Heropage component

--- src/Components/HeroPage/HeroPage.jsx
+++ src/Components/HeroPage/HeroPage.jsx
@@ -14,15 +14,29 @@ const HeroPage = () => {
                     Gallery <img src={Star} alt="Star SVG" />
                 </h2>
                 <div className='heropage-sub-details'>
-            <div className='circle'>
-                <img src={arrowDown} alt="Arrow Down Vector" />
-                <p>Discover NFT</p>
-            </div>
-            <div className='description'>
+                    <div className='circle'>
+                        <img src={arrowDown} alt="Arrow Down Vector" />
+                        <p>Discover NFT</p>
+                 </div>
+                <div className='description'>
                 <p>The Larges NFT Marketplace. Automatic and truly <br/>unique digital creation. Signed and issued by the <br/>creator, made possible by blockchain technologies</p>
+             </div>
+        </div>
+        <div className='heropage-stats'>
+            <div className='stat'>
+                <h5>Artwork</h5>
+                <h4>25.1k</h4>
+            </div>
+            <div className='stat'>
+                <h5>Artist</h5>
+                <h4>15.6k</h4>
+            </div>
+            <div className='stat'>
+                <h5>Auction</h5>
+                <h4>15.6k</h4>
             </div>
         </div>
-            </div>
+        </div>
             <div className='heropage-image'>
                 <img src={heroPageImage} alt="HeroPage Illustration" />
             </div>
