94e8f0c79c0b38325aff2292483656551be3aff4
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-17 13:11:24 +0100

    Started working on the Partners Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HeroPage from './Components/HeroPage/HeroPage';
 import NavBar from './Components/NavBar/NavBar';
+import Partners from './Components/Partners/Partners';
 
 function App() {
   return (
     <div>
       <NavBar/>
       <HeroPage/>
+      <Partners/>
     </div>
   );
 }
