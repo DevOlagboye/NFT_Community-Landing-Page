e284a5f27bacdecdde9f8203a93b87e8cf0d29e9
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-17 13:26:57 +0100

    Created Data.js to fetch Partners images

---
+++ src/Components/Partners/data.js
@@ -0,0 +1,21 @@
+import firstPartner from "../../Assets/images/coinbase.svg"
+import secondPartner from "../../Assets/images/spotify.svg"
+import thirdPartner from "../../Assets/images/slack.svg"
+import fourthPartner from "../../Assets/images/dropbox.svg"
+import fifthPartner from "../../Assets/images/webflow.svg"
+export const ourPartners = [{
+        partnerProfile: firstPartner
+    },
+    {
+        partnerProfile: secondPartner
+    },
+    {
+        partnerProfile: thirdPartner
+    },
+    {
+        partnerProfile: fourthPartner
+    },
+    {
+        partnerProfile: fifthPartner
+    }
+]
