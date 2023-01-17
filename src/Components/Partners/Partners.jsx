89e750c143ed75af1da6d13a0b859e0b04b4a8d8
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-17 13:29:41 +0100

    Partners images are fetched from the data.js

--- src/Components/Partners/Partners.jsx
+++ src/Components/Partners/Partners.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import "./Partners.css"
+import {ourPartners} from "./data"
 
 const Partners = () => {
   return (
     <div className='partners-container'>
         <div className='partners'>
-           <div>
-            
-           </div>
+           {ourPartners.map(ourPartner => (
+            <div>
+                <img src={ourPartner.partnerProfile} alt="Our partners" />
+            </div>
+           ))}
         </div>
     </div>
   )
