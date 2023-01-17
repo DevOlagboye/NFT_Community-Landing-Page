9fe69da27d128ebe5815fd5e1cf33ac668923ce3
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-17 10:58:57 +0100

    Added contents required in the the HeroPage Component

--- src/Components/HeroPage/HeroPage.jsx
+++ src/Components/HeroPage/HeroPage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Star from "../../Assets/images/Star.svg"
+import heroPageImage from "../../Assets/images/heropage-image.png"
+import "./HeroPage.css"
 
 const HeroPage = () => {
   return (
-    <div>HeroPage</div>
+    <div className='heropage-container'>
+        <div className='heropage-details'>
+            <div className='heropage-text'>
+                <h2>Create Your <br/>
+                    Own <span>NFT Dream</span><br/>
+                    Gallery <img src={Star} alt="Star SVG" />
+                </h2>
+            </div>
+            <div className='heropage-image'>
+                <img src={heroPageImage} alt="HeroPage Illustration" />
+            </div>
+        </div>
+    </div>
   )
 }
 
