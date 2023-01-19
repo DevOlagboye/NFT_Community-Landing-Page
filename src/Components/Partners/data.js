e1f965e231eacbada7cd2628e2612f37186c0844
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-19 13:09:49 +0100

    Started working on the remaining NFT cards

--- src/Components/Partners/data.js
+++ src/Components/Partners/data.js
@@ -4,18 +4,23 @@ import thirdPartner from "../../Assets/images/slack.svg"
 import fourthPartner from "../../Assets/images/dropbox.svg"
 import fifthPartner from "../../Assets/images/webflow.svg"
 export const ourPartners = [{
+        key: 1,
         partnerProfile: firstPartner
     },
     {
+        key: 2,
         partnerProfile: secondPartner
     },
     {
+        key: 3,
         partnerProfile: thirdPartner
     },
     {
+        key: 4,
         partnerProfile: fourthPartner
     },
     {
+        key: 5,
         partnerProfile: fifthPartner
     }
 ]
