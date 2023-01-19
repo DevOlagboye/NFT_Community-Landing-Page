5b6b53231b647e9c99faee98023d5b9cbacaea4b
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-19 14:18:07 +0100

    Started working on the remaining NFT cards

--- src/Components/NftCard/data.js
+++ src/Components/NftCard/data.js
@@ -8,43 +8,51 @@ import seventhNFT from "../../Assets/nfts/nft-image7.png"
 import eighthNFT from "../../Assets/nfts/nft-image8.png"
 
 export const NFTs = [{
+        key: 1,
         nftName: "ArtCrypto 01",
         nftImage: firstNFT,
         nftPrice: "0.25",
         amount: "1 of 32"
     },
     {
+        key: 2,
         nftName: "ArtCrypto 02",
         nftImage: secondNFT,
         nftPrice: "0.25",
         amount: "1 of 32"
     },
     {
+        key: 3,
         nftName: "ArtCrypto 03",
         nftImage: thirdNFT,
         nftPrice: "0.25",
         amount: "1 of 32"
     }, {
+        key: 4,
         nftName: "ArtCrypto 04",
         nftImage: fourthNFT,
         nftPrice: "0.25",
         amount: "1 of 32"
     }, {
+        key: 5,
         nftName: "ArtCrypto 05",
         nftImage: fifthNFT,
         nftPrice: "0.25",
         amount: "1 of 32"
     }, {
+        key: 6,
         nftName: "ArtCrypto 06",
         nftImage: sixthNFT,
         nftPrice: "0.25",
         amount: "1 of 32"
     }, {
+        key: 7,
         nftName: "ArtCrypto 07",
         nftImage: seventhNFT,
         nftPrice: "0.25",
         amount: "1 of 32"
     }, {
+        key: 8,
         nftName: "ArtCrypto 08",
         nftImage: eighthNFT,
         nftPrice: "0.25",
