52b4ebed3c6b87d27ec8945e598b90250255b9f4
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-19 16:53:51 +0100

    Bugs fixed now proceeding to the next component

--- src/Components/NftCard/NftCard.jsx
+++ src/Components/NftCard/NftCard.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import "./NftCard.css"
 import {Modal} from "antd"
 import ethImage from "../../Assets/images/ETH.png"
+import liveAuction from "../../Assets/images/LIVE-AUCTION.png"
 import { NFTs } from './data'
 
 const NftCard = () => {
@@ -57,6 +58,9 @@ const NftCard = () => {
                 </Modal>
                 <div>
                     <button className='view-more-btn'>View More</button>
+                </div>
+                <div className='live-auction'>
+                    <img src={liveAuction} alt="live auction" />
                 </div>
     </div>
   )
