019c84336e2caff726ba2dd7c3ce4344285bddf2
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-19 11:35:47 +0100

    Started working on the remaining NFT cards

--- src/Components/NftCard/data.js
+++ src/Components/NftCard/data.js
@@ -2,6 +2,7 @@ import firstNFT from "../../Assets/nfts/nft-image1.png"
 import secondNFT from "../../Assets/nfts/nft-image2.png"
 import thirdNFT from "../../Assets/nfts/nft-image3.png"
 import fourthNFT from "../../Assets/nfts/nft-image4.png"
+import fifthNFT from "../../Assets/nfts/nft-image5.png"
 
 export const NFTs = [{
         nftName: "ArtCrypto 01",
@@ -23,6 +24,11 @@ export const NFTs = [{
     }, {
         nftName: "ArtCrypto 04",
         nftImage: fourthNFT,
+        nftPrice: "0.25",
+        amount: "1 of 32"
+    }, {
+        nftName: "ArtCrypto 05",
+        nftImage: fifthNFT,
         nftPrice: "0.25",
         amount: "1 of 32"
     }
