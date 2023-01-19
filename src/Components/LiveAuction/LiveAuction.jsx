3d5a34445239f47c6caeda121d1f5cd171d9a745
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-19 17:03:44 +0100

    Bugs fixed now proceeding to the next component

--- src/Components/LiveAuction/LiveAuction.jsx
+++ src/Components/LiveAuction/LiveAuction.jsx
@@ -4,7 +4,7 @@ import liveAuction from "../../Assets/images/LIVE-AUCTION.png"
 
 const LiveAuction = () => {
   return (
-    <div>
+    <div className='liveauction-img'>
         <img src={liveAuction} alt="" />
     </div>
   )
