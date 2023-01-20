db72ec6f0dfbe4c9a25c9fddcfef76e72e1404cd
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-20 15:14:11 +0100

    Started working on the Creators of the Week  component

--- src/Components/Creators/Creators.jsx
+++ src/Components/Creators/Creators.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import creator1 from "../../Assets/creators/Wallet.svg"
+import "./Creators.css"
 
 const Creators = () => {
   return (
     <div className='creators-container'>
         <div className='creators-text'>
             <h3>Creators of the Week</h3>
+        </div>
+        <div className='creators-cards'>
+            <div className='creators-card'>
+                <div className='creators-card-image'>
+                    <img src={creator1} alt=""  className='creators-image'/>
+                </div>
+                <div className='creators-card-details'>
+                    <h5>Eka Prakasa</h5>
+                    <p>13.2K</p>
+                </div>
+            </div>
         </div>
     </div>
   )
