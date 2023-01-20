602da98cfc044da23ca896abe26c8c2a78390511
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-20 15:38:30 +0100

    Fetching Creators from the data.js

---
+++ src/Components/Creators/data.js
@@ -0,0 +1,11 @@
+import creator1 from "../../Assets/creators/Wallet.svg"
+
+export const topCreator = [{
+    creatorName: "Eka Prakasa",
+    creatorStats: "13.2K",
+    creatorProfile: creator1
+},{
+    creatorName: "Eka Prakasa",
+    creatorStats: "13.2K",
+    creatorProfile: creator1
+}]
