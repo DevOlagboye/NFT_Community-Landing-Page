27d5c149ad3107c80174c9297f3d4d6a22314779
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-20 13:44:20 +0100

    Started working on the SellNFT component

--- src/Components/SellNft/SellNft.jsx
+++ src/Components/SellNft/SellNft.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import walletImage from "../../Assets/images/Wallet.png"
 import "./SellNft.css"
 
 const SellNft = () => {
@@ -6,6 +7,13 @@ const SellNft = () => {
     <div className='sell-nft-container'>
         <div className='sell-nft-text'>
             <h3>Create Your Sell NFT</h3>
+        </div>
+        <div className='sell-nft-cards'>
+            <div className='sell-nft-card'>
+                <img src={walletImage} alt="" />
+                <h5>Connect Wallet</h5>
+                <p>This growth plan will help you reach <br/> your resolutions and achieve the <br/>goals you have been striving <br/>towards.</p>
+            </div>
         </div>
     </div>
   )
