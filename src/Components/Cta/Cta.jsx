7bfc8434fcdc95645256c5fa29d37f94ad86dc12
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-22 16:17:39 +0100

    Started working on the Call to Action Component

--- src/Components/Cta/Cta.jsx
+++ src/Components/Cta/Cta.jsx
@@ -4,7 +4,10 @@ import "./Cta.css"
 const Cta = () => {
   return (
     <div className='cta-container'>
-
+        <div className='cta-details'>
+            <h3>Join Our Community</h3>
+            <button>Get Started</button>
+        </div>
     </div>
   )
 }
