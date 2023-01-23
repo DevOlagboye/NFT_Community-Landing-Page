b31eef2bbb550f671f641410b1ee72c37a1bebca
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-23 10:49:09 +0100

    Installed react icons

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Footer.css"
+import { FaTwitter, FaLinkedin, FaFacebook, FaGithub } from "react-icons/fa";
 import footerLogo from "../../Assets/images/ApolloNFT.png"
 
 const Footer = () => {
@@ -12,7 +13,10 @@ const Footer = () => {
               <h5>This growth plan will help you reach<br/> your resolutions and achieve the goals <br/> you have been striving towards.</h5>
             </div>
             <div className='footer-social-icons'>
-              
+                <FaTwitter/>
+                <FaLinkedin/>
+                <FaFacebook/>
+                <FaGithub/>
             </div>
           </div>
       </div>
