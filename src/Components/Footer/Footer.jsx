03c856b159309c99af622710e0a37a9dd470f6a9
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-23 10:28:32 +0100

    Started working on the Footer Component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./Footer.css"
+import footerLogo from "../../Assets/images/ApolloNFT.png"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <div className='footer-details'>
+          <div className='footer-logo-text'>
+            <div className='footer-logo'>
+              <img src={footerLogo} alt="Apollo Logo" />
+              <h5>This growth plan will help you reach<br/> your resolutions and achieve the goals <br/> you have been striving towards.</h5>
+            </div>
+            <div className='footer-social-icons'>
+              
+            </div>
+          </div>
+      </div>
+    </div>
   )
 }
 
