d8d5370beb891288bb5dad584e574d44b400a0f2
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-24 15:34:51 +0100

    Working on the Responsiveness

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,8 +2,16 @@ import React , {useState}  from 'react'
 import { Drawer } from 'antd'
 import "./NavBar.css"
 import NftLogo from "../../Assets/images/ApolloNFT.png"
+import {FaBars} from "react-icons/fa"
 
 const NavBar = () => {
+    const [open, setOpen] = useState(false);
+    const showMenu = () =>{
+        setOpen(true)
+    }
+    const hideMenu = () =>{
+        setOpen(false)
+    }
   return (
     <div className='navbar-container'>
         <div className='logo'>
@@ -16,12 +24,21 @@ const NavBar = () => {
                 <li>Creator</li>
                 <li>Community</li>
             </ul>
-
-
         </div>
         <div className='nav-button'>
             <button>Contact Us</button>
         </div>
+        <button onClick={showMenu} className="">
+                <FaBars/>
+            </button>
+            <Drawer placement='left' onClose={hideMenu} open={open}>
+            <ul>
+                <li>Drop</li>
+                <li>Marketplace</li>
+                <li>Creator</li>
+                <li>Community</li>
+            </ul>
+            </Drawer>
     </div>
   )
 }
