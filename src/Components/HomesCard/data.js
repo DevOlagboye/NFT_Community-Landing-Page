541a188c2067b36d3570bd42906f4987f2bc184f
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-23 13:43:03 +0100

    Started Working on the Homes NFT

---
+++ src/Components/HomesCard/data.js
@@ -0,0 +1,62 @@
+import firstNFT from "../../Assets/nfts/nft-image1.png"
+import secondNFT from "../../Assets/nfts/nft-image2.png"
+import thirdNFT from "../../Assets/nfts/nft-image3.png"
+import fourthNFT from "../../Assets/nfts/nft-image4.png"
+import fifthNFT from "../../Assets/nfts/nft-image5.png"
+import sixthNFT from "../../Assets/nfts/nft-image6.png"
+import seventhNFT from "../../Assets/nfts/nft-image7.png"
+import eighthNFT from "../../Assets/nfts/nft-image8.png"
+
+export const NFTs = [{
+        key: 1,
+        nftName: "ArtCrypto 01",
+        nftImage: firstNFT,
+        nftPrice: "0.25",
+        amount: "1 of 32"
+    },
+    {
+        key: 2,
+        nftName: "ArtCrypto 02",
+        nftImage: secondNFT,
+        nftPrice: "0.25",
+        amount: "1 of 32"
+    },
+    {
+        key: 3,
+        nftName: "ArtCrypto 03",
+        nftImage: thirdNFT,
+        nftPrice: "0.25",
+        amount: "1 of 32"
+    }, {
+        key: 4,
+        nftName: "ArtCrypto 04",
+        nftImage: fourthNFT,
+        nftPrice: "0.25",
+        amount: "1 of 32"
+    }, {
+        key: 5,
+        nftName: "ArtCrypto 05",
+        nftImage: fifthNFT,
+        nftPrice: "0.25",
+        amount: "1 of 32"
+    }, {
+        key: 6,
+        nftName: "ArtCrypto 06",
+        nftImage: sixthNFT,
+        nftPrice: "0.25",
+        amount: "1 of 32"
+    }, {
+        key: 7,
+        nftName: "ArtCrypto 07",
+        nftImage: seventhNFT,
+        nftPrice: "0.25",
+        amount: "1 of 32"
+    }, {
+        key: 8,
+        nftName: "ArtCrypto 08",
+        nftImage: eighthNFT,
+        nftPrice: "0.25",
+        amount: "1 of 32"
+
+    }
+]
