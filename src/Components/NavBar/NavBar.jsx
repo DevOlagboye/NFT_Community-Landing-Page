5af974dbe3f965fe8a3429333095bef9b8831925
Olagboye Seyi <akingbolaola@gmail.com>
2023-06-16 19:07:32 +0100

    Updated the navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,47 +1,49 @@
-import React , {useState}  from 'react'
-import { Drawer } from 'antd'
-import "./NavBar.css"
-import NftLogo from "../../Assets/images/ApolloNFT.png"
-import {FaBars} from "react-icons/fa"
+import React, { useState } from "react";
+import { Drawer } from "antd";
+import "./NavBar.css";
+import NftLogo from "../../Assets/images/ApolloNFT.png";
+import { FaBars } from "react-icons/fa";
 
 const NavBar = () => {
-    const [open, setOpen] = useState(false);
-    const showMenu = () =>{
-        setOpen(true)
-    }
-    const hideMenu = () =>{
-        setOpen(false)
-    }
+  const [open, setOpen] = useState(false);
+  const showMenu = () => {
+    setOpen(true);
+  };
+  const hideMenu = () => {
+    setOpen(false);
+  };
   return (
-    <div className='navbar-container'>
-        <div className='logo'>
-            <img src={NftLogo} alt="Apollo NFT Logo" />
-        </div>
-        <div className='nav-list'>
-            <ul>
-                <li>Drop</li>
-                <li>Marketplace</li>
-                <li>Creator</li>
-                <li>Community</li>
-            </ul>
-        </div>
-        <div className='nav-button'>
-            <button>Contact Us</button>
-        </div>
-        <button onClick={showMenu} className="nav-bar-icon">
-                <FaBars/>
-            </button>
-            <Drawer placement='left' onClose={hideMenu} open={open} className="mobile-menu">
-            <ul>
-                <li>Drop</li>
-                <li>Marketplace</li>
-                <li>Creator</li>
-                <li>Community</li>
-                <button>Connect Wallet</button>
-            </ul>
-            </Drawer>
+    <div className="navbar-container">
+      <div className="logo">
+        <img src={NftLogo} alt="Apollo NFT Logo" />
+      </div>
+      <div className="nav-list">
+        <ul>
+          <li>Marketplace</li>
+          <li>Creator</li>
+        </ul>
+      </div>
+      <div className="nav-button">
+        <button>Connect Wallet</button>
+      </div>
+      <button onClick={showMenu} className="nav-bar-icon">
+        <FaBars />
+      </button>
+      <Drawer
+        placement="left"
+        onClose={hideMenu}
+        open={open}
+        className="mobile-menu"
+      >
+        <ul>
+          <li>Marketplace</li>
+          <li>Creator</li>
+          <li>Community</li>
+          <button>Connect Wallet</button>
+        </ul>
+      </Drawer>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
