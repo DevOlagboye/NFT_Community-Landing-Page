fcdbb1bcb4717ba158ed7a023d3ce94b072d9165
Olagboye Seyi <akingbolaola@gmail.com>
2024-03-28 20:39:03 +0100

    Updated live Auction

--- src/Components/LiveAuction/LiveAuction.jsx
+++ src/Components/LiveAuction/LiveAuction.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import "./LiveAuction.css"
-import liveAuction from "../../Assets/images/LIVE-AUCTION.png"
+import React from "react";
+import "./LiveAuction.css";
+import Marquee from "react-fast-marquee";
+import liveAuction from "../../Assets/images/LIVE-AUCTION.png";
 
+const text = "LIVE AUCTION.";
 const LiveAuction = () => {
   return (
-    <div className='liveauction-img'>
-        <img src={liveAuction} alt="" />
+    <div className="liveauction-img">
+      <div className="live-auction-text">
+        <Marquee>
+          <div className="texts">
+            <p>{text}</p> <p>{text}</p>
+          </div>
+        </Marquee>
+      </div>
+      {/* <img src={liveAuction} alt="" /> */}
     </div>
-  )
-}
+  );
+};
 
-export default LiveAuction
+export default LiveAuction;
